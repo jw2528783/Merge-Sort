41d9f7fbbb609eeb71e795f506dd0a70caf661f0
Justin Waterman <jw2528783@users.noreply.github.com>
2017-10-13 11:52:37 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,96 +3,69 @@
 
 using namespace std;
 
-void merge(int a[],int n){
-	int i,j,k;
-	int m=n/2;
-	int n1=m+1;
-	int n2=n-m;
-	int temp[n1],temp2[n2];
-	for(int i=0;i<n1;i++)temp[i]=a[i];
-	for(int j=0;j<n2;j++)temp2[j]=a[m+1+j];
-	i=0,j=0,k=0;
-	while(i<n1 && j<n2){
-		if(temp[i]<=temp2[j]){
-			a[k]=temp[i];
-			i++;
-		}
-		else{
-			a[k]=temp2[j];
-			j++;
-		}
-		k++;
-	}
-	while(i<n1){
-		a[k]=temp[i];
-		i++;
-		k++;
-	}
-	while(j<n2){
-		a[k]=temp2[j];
-		j++;
-		k++;
-	}
+// s = start of range to sort
+// e = end of range to sort
+void msort(int a[], int x[], int s, int e) {
+    int m=(s+e)/2;
+    if(e-s<1)return;
+    msort(a,x,s,m);
+    msort(a,x,m+1,e);
+    int i=s,j=m+1,k=0;
+    while(i<=m && j<=e){
+        if(a[i]<a[j]){
+            x[k]=a[i];
+            i++;
+            k++;
+        }
+        else{
+            x[k]=a[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=m){
+        x[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<=e){
+        x[k]=a[j];
+        j++;
+        k++;
+    }
+    for(int i=s;i<=e;i++){
+        a[i]=x[i-s];
+    }
 }
 
-void mergesort(int a[],int start, int n){
-	if(0<n){
-		mergesort(a,start,n/2);
-		mergesort(a,(n/2)+1,n);
-		merge(a,n);
-	}
+void mergesort(int a[], int n) {
+    int *x=new int[n];
+    msort(a,x,0,n-1);
+    delete[] x;
 }
-
 
 bool sorted(int a[], int n) {
 	for (int i = 0; i < n - 1; ++i) {
-		if (a[i] > a[i + 1]) return false;
+            if (a[i] > a[i + 1])return false;
 	}
 	return true;
 }
 
-void printA(int a[],int n){
-	for(int i=0;i<n;i++){
-		cout<<a[i]<<" ";
-	}
-	cout<<endl<<endl;
-}
-
 int main(int argc, char * args[]) {
-	int a[] = { 2, -9, 122, 45, -7, 33, -7, 16, 99, 21 };
-	printA(a,10);
-	mergesort(a, 0, 10);
-	printA(a,10);
-	/*assert(a[0] == -9);
-	assert(a[1] == -7);
-	assert(a[2] == -7);
-	assert(a[3] == 2);
-	assert(a[4] == 16);
-	assert(a[5] == 21);
-	assert(a[6] == 33);
-	assert(a[7] == 45);
-	assert(a[8] == 99);
-	assert(a[9] == 122);
-	/*
-	int b[] = { 2 };
-	quicksort(b, 0, 0);
-	assert(b[0] == 2);
+	int a[1000];
+	for (int i = 0; i < 1000; ++i) a[i] = rand() % 100;
+	mergesort(a, 1000);
+	assert(sorted(a, 1000));
+	int b[1001];
+	for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
+	mergesort(b, 1001);
+	assert(sorted(b, 1001));
+	int c[] = { 2 };
+	mergesort(c, 1);
+	assert(sorted(c, 1));
+	int d[] = { 1, 2, 3, 4, 5 };
+	mergesort(d, 5);
+	assert(sorted(d, 5));
 
-	int c[] = { 2, 2 };
-	quicksort(c, 0, 1);
-	assert(c[0] == 2);
-	assert(c[1] == 2);
-
-	int d[] = { 2, -2 };
-	quicksort(d, 0, 1);
-	assert(d[0] == -2);
-	assert(d[1] == 2);
-
-	srand(time(0));
-	int e[1000];
-	for (int i = 0; i < 1000; ++i) e[i] = rand() % 100;
-	quicksort(e, 0, 999);
-	assert(sorted(e, 1000));
-	*/
 	cout << "All tests passed." << endl;	
 }
